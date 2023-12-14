719a03d210b601efaa8f956ecd753c46b86716ab
iman fatahi <149057251+Imanfd84@users.noreply.github.com>
2023-12-14 21:35:51 +0330

    making classes better to read

---
+++ Farm/Animal.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+public abstract class Animal
+{
+    public double Weight {  get; set; }
+    public bool IsMale {  get; set; }
+    public string Name { get; set; }
+
+    public DateTime BirthDate { get; set; } 
+    public static int TotalNumber { get; private set; }
+    public int Number { get; private set; }
+
+    static Animal()
+    {
+        TotalNumber = 0;
+    }
+    public Animal()
+    {
+        TotalNumber++;
+        Number = TotalNumber;
+    }
+
+
+    public abstract int Life();
+    public abstract double StressFactor();
+    public abstract int TimeToDie();
+    public abstract float KillPriority();
+    public abstract int CostPerDay();
+    public abstract int ValuePerDay();
+}
+
+
